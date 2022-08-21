7da218e2fa1803ab9cc94dee148e9ac220f4bb49
sijiaw96 <109883695+sijiaw96@users.noreply.github.com>
2022-08-20 22:28:42 -0500

    Update user-research.php

--- user-research.php
+++ user-research.php
@@ -49,22 +49,9 @@
 		</div>
 	</nav>
     <div class = "services">
+	    
         <?php
-//         $servername = "localhost";
-//         $username = "root";
-//         $password = "";
-//         $dbname = "bus670";
-
-//         // Create connection
-//         $conn = new mysqli($servername, $username, $password, $dbname);
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-	$server = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$db = substr($url["path"], 1);
-
-	$conn = new mysqli($server, $username, $password, $db);
+	include "cleardb_connection.php";
         // Check connection
         if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
