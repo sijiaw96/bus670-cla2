6e11aaec3595abf28d9c6a1f62704b43cf7a0ca2
sijiaw96 <109883695+sijiaw96@users.noreply.github.com>
2022-08-20 22:24:10 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,13 +3,13 @@
 <html>
 <head>
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="description" content="$1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" type="text/css" href="style.css">
-
-<title>test</title>
+ -->
+<title>insert</title>
 
 
 </head>
