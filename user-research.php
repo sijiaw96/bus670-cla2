f0b5e92e1dbd20dc0421e36cc1b41e841ddbb01d
sijiaw96 <109883695+sijiaw96@users.noreply.github.com>
2022-08-20 22:12:56 -0500

    cleardb config

--- user-research.php
+++ user-research.php
@@ -50,13 +50,21 @@
 	</nav>
     <div class = "services">
         <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "bus670";
+//         $servername = "localhost";
+//         $username = "root";
+//         $password = "";
+//         $dbname = "bus670";
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+//         // Create connection
+//         $conn = new mysqli($servername, $username, $password, $dbname);
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = new mysqli($server, $username, $password, $db);
         // Check connection
         if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
