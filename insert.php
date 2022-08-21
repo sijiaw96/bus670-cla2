8c8f668a90a6fdc9fb5fdc70787dd4a79ffb6386
sijiaw96 <109883695+sijiaw96@users.noreply.github.com>
2022-08-20 22:27:51 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -16,15 +16,7 @@
 <body>
 
  <?php
-// $conn = new mysqli("localhost", "root", "", "bus670");
- $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-	$server = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$db = substr($url["path"], 1);
-
-	$conn = new mysqli($server, $username, $password, $db);
+include "cleardb_connection.php";
 // Check connection
  
 if($conn === false){
